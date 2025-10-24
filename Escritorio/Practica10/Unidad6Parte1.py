3b5d5779b2813885bc608aaed9084ddb06c0b6bf
in <mignaciop2004@gmail.com>
2025-10-24 02:14:52 -0400

    Es La Practica 10

---
+++ Escritorio/Practica10/Unidad6Parte1.py
@@ -0,0 +1,12 @@
+
+mapa_bolivia = {
+    'La Paz': ['Oruro', 'Beni'],
+    'Oruro': ['La Paz', 'Cochabamba', 'Potosi'],
+    'Cochabamba': ['Oruro', 'Santa Cruz', 'Chuquisaca', 'Beni'],
+    'Potosi': ['Oruro', 'Chuquisaca', 'Tarija'],
+    'Chuquisaca': ['Cochabamba', 'Potosi', 'Santa Cruz', 'Tarija'],
+    'Santa Cruz': ['Cochabamba', 'Chuquisaca', 'Beni'],
+    'Tarija': ['Potosi', 'Chuquisaca'],
+    'Beni': ['La Paz', 'Cochabamba', 'Santa Cruz'],
+    'Pando': [] # Pando está aislado en este mapa simplificado
+}
